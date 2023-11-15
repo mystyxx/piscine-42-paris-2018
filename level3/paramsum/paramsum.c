8235fd89013c8789e280f35d18d946fb5d78f3b8
Lya Roux <paul.rouxb@pm.me>
2023-11-15 14:37:20 +0100

    solved level3/paramsum

---
+++ level3/paramsum/paramsum.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char c) {
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int n) {
+	if(n>=10) {
+		ft_putnbr(n/10);
+	}
+	ft_putchar((n%10) + '0');
+	
+}
+
+
+int main(int argc, char **argv) {
+	ft_putnbr(argc-1);
+	ft_putchar('\n');
+	return 0;
+}
